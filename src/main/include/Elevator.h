2040144278a2495356263f32374853971ca78c01
Dean Martin <deanmartin@830>
2019-02-19 23:55:57 -0500

    very dumb bug

--- src/main/include/Elevator.h
+++ src/main/include/Elevator.h
@@ -30,7 +30,6 @@ class Elevator {
         static constexpr double FIRST_BALL_HEIGHT = 27.5 + FLYWHEEL_OFFSET;
         static constexpr double SECOND_BALL_HEIGHT = FIRST_BALL_HEIGHT + DIVIDER_DISTANCE;
         static constexpr double THIRD_BALL_HEIGHT = SECOND_BALL_HEIGHT + DIVIDER_DISTANCE;
-w
         double p = 0 ,i = 0 ,d = 0,f = 0.0;
         bool motorFlipped = false;
         bool encoderFlipped = false;
