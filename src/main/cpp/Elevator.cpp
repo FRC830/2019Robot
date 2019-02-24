1e6cdde8a173c3067d2259711dd63a4b1b0bd417
Joey Bishop <JoooooooeyBishop@FRC.830>
2019-02-24 14:25:59 -0500

    added persistent auto-heights (not tested)

--- src/main/cpp/Elevator.cpp
+++ src/main/cpp/Elevator.cpp
@@ -14,8 +14,22 @@ Elevator::Elevator(WPI_TalonSRX &motor) : motor(motor) {
     nt_i = Shuffleboard::GetTab("Robot Specific").AddPersistent("ELEV I", i).GetEntry();
     nt_d = Shuffleboard::GetTab("Robot Specific").AddPersistent("ELEV D", d).GetEntry();
     nt_f = Shuffleboard::GetTab("Robot Specific").AddPersistent("ELEV F", f).GetEntry();
+
+    nt_fhh=Shuffleboard::GetTab("Robot Specific").AddPersistent("BOTTOM HATCH", first_hatch_height).GetEntry();
+    nt_fbh=Shuffleboard::GetTab("Robot Specific").AddPersistent("BOTTOM BALL", first_ball_height).GetEntry();
+    nt_shh=Shuffleboard::GetTab("Robot Specific").AddPersistent("MIDDLE HATCH", second_hatch_height).GetEntry();
+    nt_sbh=Shuffleboard::GetTab("Robot Specific").AddPersistent("MIDDLE BALL", second_ball_height).GetEntry();
+    nt_thh=Shuffleboard::GetTab("Robot Specific").AddPersistent("TOP BALL", third_hatch_height).GetEntry();
+    nt_tbh=Shuffleboard::GetTab("Robot Specific").AddPersistent("TOP HATCH", third_ball_height).GetEntry();
 }
 void Elevator::update() {
+    heights[0] = nt_fhh.GetDouble(first_hatch_height);
+    heights[1] = nt_fbh.GetDouble(first_ball_height);
+    heights[2] = nt_shh.GetDouble(second_hatch_height);
+    heights[3] = nt_sbh.GetDouble(second_ball_height);
+    heights[4] = nt_thh.GetDouble(third_hatch_height);
+    heights[5] = nt_tbh.GetDouble(third_ball_height);
+
     motor.SetSensorPhase(nt_encoderFlipped.GetBoolean(encoderFlipped));
     motor.SetInverted(nt_motorFlipped.GetBoolean(motorFlipped));
     motor.Config_kP(0, nt_p.GetDouble(p));
--- src/main/include/Elevator.h
+++ src/main/include/Elevator.h
@@ -24,15 +24,16 @@ class Elevator {
         static constexpr double DIVIDER_DISTANCE = 28;
         
         // Heights of all targets, In inches
-        static constexpr double FIRST_HATCH_HEIGHT = 19 + SPEAR_OFFSET;
-        static constexpr double SECOND_HATCH_HEIGHT = FIRST_HATCH_HEIGHT + DIVIDER_DISTANCE;
-        static constexpr double THIRD_HATCH_HEIGHT = SECOND_HATCH_HEIGHT + DIVIDER_DISTANCE;
+        nt::NetworkTableEntry nt_fhh, nt_shh, nt_thh, nt_fbh, nt_sbh, nt_tbh; // have fun deciphering this
+        double first_hatch_height = 19 + SPEAR_OFFSET;
+        double second_hatch_height = first_hatch_height + DIVIDER_DISTANCE;
+        double third_hatch_height = second_hatch_height + DIVIDER_DISTANCE;
 
-        static constexpr double FIRST_BALL_HEIGHT = 27.5 + FLYWHEEL_OFFSET;
-        static constexpr double SECOND_BALL_HEIGHT = FIRST_BALL_HEIGHT + DIVIDER_DISTANCE;
-        static constexpr double THIRD_BALL_HEIGHT = SECOND_BALL_HEIGHT + DIVIDER_DISTANCE;
+        double first_ball_height = 27.5 + FLYWHEEL_OFFSET;
+        double second_ball_height = first_ball_height + DIVIDER_DISTANCE;
+        double third_ball_height = second_ball_height + DIVIDER_DISTANCE;
         // defaults
-        double p = 0 ,i = 0 ,d = 0,f = 0.0;
+        double p = 0.02 ,i = 0 ,d = 0,f = 0.0;
 
         bool motorFlipped = false;
         bool encoderFlipped = false;
@@ -40,11 +41,11 @@ class Elevator {
         nt::NetworkTableEntry nt_p, nt_i, nt_d, nt_f, nt_motorFlipped, nt_encoderFlipped;
 
         std::vector<double> heights = {
-            FIRST_HATCH_HEIGHT, 
-            FIRST_BALL_HEIGHT, 
-            SECOND_HATCH_HEIGHT, 
-            SECOND_BALL_HEIGHT, 
-            THIRD_HATCH_HEIGHT, 
-            THIRD_BALL_HEIGHT};
+            first_hatch_height, 
+            first_ball_height, 
+            second_hatch_height, 
+            second_ball_height, 
+            third_hatch_height, 
+            third_ball_height};
 
 };
